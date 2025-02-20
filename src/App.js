655396f043bc8a8b023704318cd585adbcedfcfd
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2025-02-20 13:59:42 -0600

    icon changed to initials.svg

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Portfolio } from "./pages/PortfolioPages/PortfolioRoutes";
-import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 import PotteryPortfolio from "./pages/PortfolioPages/PotteryPortfolio";
 import WebDevPortfolio from "./pages/PortfolioPages/WebDevPortfolio";
